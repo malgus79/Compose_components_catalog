7868d97c2f27ffb6b77dd9d04118665feb2daf33
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-30 20:39:15 -0300

    Visibility animation added

--- app/src/main/java/com/cursokotlin/jetpackcomponentscatalog/AnimationExamples.kt
+++ app/src/main/java/com/cursokotlin/jetpackcomponentscatalog/AnimationExamples.kt
@@ -1,17 +1,17 @@
 package com.cursokotlin.jetpackcomponentscatalog
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -44,12 +44,39 @@ fun SizeAnimation() {
     var smallSize by rememberSaveable { mutableStateOf(true) }
     val size by animateDpAsState(targetValue = if (smallSize) 50.dp else 100.dp,
         animationSpec = tween(durationMillis = 500),
-        finishedListener = {if(!smallSize){} })
+        finishedListener = {
+            if (!smallSize) {
+            }
+        })
     Box(modifier = Modifier
         .size(size)
         .background(Color.Cyan)
         .clickable { smallSize = !smallSize })
 }
+
+//hacer una columna con un boton y un box. Cada vez que se pulse en el btn se muestra/oculta le box
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun VisibilityAnimation() {
+
+    var isVisible by remember { mutableStateOf(true) }
+
+    Column(Modifier.fillMaxSize()) {
+        Button(onClick = { isVisible = !isVisible }) {
+            Text(text = "mostrar/Ocultar")
+        }
+        Spacer(modifier = Modifier.size(50.dp))
+
+        AnimatedVisibility (isVisible) {
+            Box(
+                Modifier
+                    .size(150.dp)
+                    .background(Color.Red)
+            )
+        }
+    }
+}
+
 
 /*
 setContent {
